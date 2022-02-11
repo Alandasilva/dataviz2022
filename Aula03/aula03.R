81c8f507f7a7d5d1bc6fc8d2effca697b5b0491a
Alandasilva <alan.silva.6991@gmail.com>
2022-02-10 23:55:48 -0300

    Add aula03

--- Aula03/aula03.R
+++ Aula03/aula03.R
@@ -11,6 +11,15 @@ library(viridis)
 # Pacote para gerar correlogramas
 install.packages("corrplot")
 library(corrplot)
+install.packages("gapminder")
+library(gapminder)
+# install.packages("devtools")
+devtools::install_github("thomasp85/patchwork")
+library(patchwork)
+
+### Gráfico de bolhas ###
+data(gapminder)
+glimpse(gapminder)
 
 ### Gráfico de mapa de árvore ###
 
@@ -62,10 +71,12 @@ happy %>%
   count(age, year) %>% # conta o num de pessoas por idade por ano
   ggplot(aes(age, year, fill = n)) +
   geom_tile() +
-  scale_fill_viridis()
+  scale_fill_viridis()+
+  theme_minimal()
+
 
 # frequencia relativa das idades
-happy %>%
+df <- happy %>%
   count(age, year) %>%
   group_by(year) %>%
   mutate(total = sum(n), # conta o total de pessoas por ano
@@ -98,9 +109,12 @@ corrplot(m, method = "color",
 corrplot(m, method = "color",
          addCoef.col = "black")
 
+corrplot(m, order = 'AOE', col = COL2('RdBu', 10))
+
 ### Matriz de gráficos ###
 
 # passando a base toda
+df_mtcars <- mtcars
 mtcars %>%
   ggpairs()
 
@@ -114,3 +128,82 @@ mtcars %>%
   select(drat:am) %>%
   mutate_at(c("vs", "am"), as_factor) %>%
   ggpairs()
+
+
+### Gráfico de cascada ###
+
+#dados
+balance <- data.frame(event =
+                        c("Starting\nCash", "Sales", "Refunds",
+                          "Payouts", "Court\nLosses", "Court\nWins", "Contracts", "End\nCash"),
+                      change = c(2000, 3400, -1100, -100, -6600, 3800, 1400, 2800))
+
+# o saldo depois de cada movimentacão financeira, usado para traçar o ponto de partida da barra
+balance$balance <- cumsum(c(0, balance$change[-nrow(balance)]))
+# o tempo em que cada movimentacao financeira foi feita, para mantermos a ordem delas
+balance$time <- 1:nrow(balance)
+# a direcao do fluxo financeiro: aumento ou diminuicao
+balance$flow <- factor(sign(balance$change))
+
+
+ggplot(balance) +
+  geom_rect(aes(xmin = time - 0.45, xmax = time + 0.45,
+                ymin = balance, ymax = balance + change, fill = flow)) +
+  geom_text(aes(x = time, y = pmin(balance, balance + change) - 50, label = paste0("$",change))) +
+  scale_y_continuous("Balance", labels=scales::dollar_format()) +
+  theme(legend.position = "none")
+
+# usando o pacote waterfall que simplifica bastante a criacão desse gráfico
+
+balance <- data.frame(event =
+                        c("Starting\nCash", "Sales", "Refunds",
+                          "Payouts", "Court\nLosses", "Court\nWins", "Contracts", "End\nCash"),
+                      change = c(2000, 3400, -1100, -100, -6600, 3800, 1400, 2800))
+
+#install.packages("waterfalls")
+library(waterfalls)
+waterfall(balance)
+waterfall(balance, calc_total = TRUE)
+
+waterfall(balance, calc_total = TRUE) +
+  labs(title = "Fluxo de caixa",
+       subtitle = "Fevereiro de 2022",
+       x = "Descrição",
+       y = "Balanço") +
+  theme_minimal()
+
+### Pacote patchwork ####
+
+df <- mtcars
+p <- ggplot(df)
+
+a <- p +
+  aes(mpg, disp, col = as.factor(vs)) +
+  geom_smooth(se = F) +
+  geom_point()
+
+b <- p +
+  aes(disp, gear, group = gear) +
+  geom_boxplot()
+
+c <- p +
+  aes(hp) +
+  stat_density(geom = "area") +
+  coord_cartesian(expand = 0)
+
+# adiciona ggplots adicionando-os com +
+a + b + c
+
+# | especifica adição lado a lado
+a | c
+
+# / é para adicionar gráficos sob o gráfico anterior
+b / c
+
+# usando parênteses para agrupar gráficos e +, | e / para adicionar os grupos
+(a | b) / c
+
+# usando parênteses para agrupar gráficos e +, | e / para adicionar os grupos
+(a | b) / c +
+  plot_annotation(tag_levels = "A") &
+  theme(legend.position = "none")
